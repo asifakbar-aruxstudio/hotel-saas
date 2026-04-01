0b5e6a3337bf7e9e5fb2ccbb546600921f055191
Asif Akbar <786asifakbar53@gmail.com>
2026-04-02 01:21:48 +0500

    add database connection logic and constants for MongoDB integration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,14 @@
-// config/db.js
 import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
 
-export const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB Connected");
-};
+
+const connectDB = async ()=>{
+    try{
+         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+            console.log(`MONGODB CONNECTED SUCCESSFULLY !! DB HOST: ${connectionInstance.connection.host}`);
+    }catch (error) {
+        console.log("MONGODB CONNECTION ERROR " , error);
+        process.exit(1);
+    }
+}  
+export default connectDB;
